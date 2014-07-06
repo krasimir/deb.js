922ffeb90c160712e3d499adf0ae06ec5d7d4c11
Tyler Eich <tyler2eich@gmail.com>
2014-07-06 00:32:14 -0400

    Overhauled Chrome extension
    Removed popup
    Button automatically refreshes page
    Per-page awareness of extension state

--- chrome/js/background.js
+++ chrome/js/background.js
@@ -1,7 +1,41 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	if(request.debjs && request.debjs && request.debjs.status === true) {
-    	chrome.browserAction.setBadgeText({ text: 'ON'});
-	} else {
-		chrome.browserAction.setBadgeText({ text: '' });
+function updateBadgeText(tabId, toggle) {
+	if (typeof tabId === 'undefined') return;
+
+	chrome.tabs.sendMessage(tabId, 'getState', // Get state
+	function(response) { // When state is retrieved...
+		var state = response;
+
+		if (toggle) state = !state;
+		var badgeText = state ? 'ON' : '';
+
+		chrome.browserAction.setBadgeText({ // Update badge
+			text: badgeText
+		});
+
+		if (toggle) {
+			chrome.tabs.sendMessage(tabId, { // Save state
+				setState: badgeText
+			}, function() { // When state is saved...
+				if (toggle) {
+					chrome.tabs.reload();
+				}
+			});
+		}
+	});
+}
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+	updateBadgeText(tab.id, true); // Toggle value
+});
+
+chrome.tabs.onActivated.addListener(function(info) {
+	updateBadgeText(info.tabId);
+});
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+
+	if (message === 'ready') {
+		sendResponse();
+		chrome.tabs.sendMessage(sender.tab.id, 'inject');
 	}
 });
