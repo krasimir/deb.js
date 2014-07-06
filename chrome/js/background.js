6b4f7df65d09d1c2b28391928c6d274c09811466
Tyler Eich <tyler2eich@gmail.com>
2014-07-06 09:13:13 -0400

    Bug fix, template update
    Inject content script before all other scripts
    Removed unnecessary communication between background and content scripts
    Comments for clarity

--- chrome/js/background.js
+++ chrome/js/background.js
@@ -1,5 +1,5 @@
 function updateBadgeText(tabId, toggle) {
-	if (typeof tabId === 'undefined') return;
+	if (typeof tabId !== 'number') return; // tabId is required
 
 	chrome.tabs.sendMessage(tabId, 'getState', // Get state
 	function(response) { // When state is retrieved...
@@ -16,26 +16,16 @@ function updateBadgeText(tabId, toggle) {
 			chrome.tabs.sendMessage(tabId, { // Save state
 				setState: badgeText
 			}, function() { // When state is saved...
-				if (toggle) {
-					chrome.tabs.reload();
-				}
+				chrome.tabs.reload(); // ...refresh page
 			});
 		}
 	});
 }
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-	updateBadgeText(tab.id, true); // Toggle value
+chrome.browserAction.onClicked.addListener(function(tab) { // On button click...
+	updateBadgeText(tab.id, true); // ...(param 2 `true` => toggle value)
 });
 
-chrome.tabs.onActivated.addListener(function(info) {
-	updateBadgeText(info.tabId);
-});
-
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-
-	if (message === 'ready') {
-		sendResponse();
-		chrome.tabs.sendMessage(sender.tab.id, 'inject');
-	}
+chrome.tabs.onActivated.addListener(function(info) { // On active tab change...
+	updateBadgeText(info.tabId); // ...update badge
 });
--- chrome/js/content.tpl.js
+++ chrome/js/content.tpl.js
@@ -1,10 +1,22 @@
-if(/\bdebjs\b/.test(location.search)) {
-    var s = document.createElement('script');
-	s.setAttribute('type', 'text/javascript');
-	s.innerHTML = '{debjs}';
-	var root = document.head || document.documentElement;
-	root.insertBefore(s, root.firstChild);
-	chrome.runtime.sendMessage({ debjs: {status: true}}, function(response) {});
-} else {
-	chrome.runtime.sendMessage({ debjs: {status: false}}, function(response) {});
+// Function to inject script into top of <head> or <html>
+function injectScript() {
+  var s = document.createElement('script');
+  s.textContent = '{debjs}';
+
+  var root = document.head || document.documentElement;
+  var newScript = root.insertBefore(s, root.firstChild);
+  s.parentNode.removeChild(s);
 }
+
+// This logic decides if deb.js should be injected or not.
+if (sessionStorage.debJsState) injectScript();
+
+// Listen for messages from background script
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  if (message === 'getState') { // Get debJsState
+    sendResponse(sessionStorage.debJsState);
+  } else if (typeof message.setState === 'string') { // Set debJsState
+    sessionStorage.debJsState = message.setState;
+    sendResponse();
+  }
+});
